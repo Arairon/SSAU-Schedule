130fbb83e45fa598797d55daf897d2105e4d7604
arairon <arai@arai.icu>
2026-03-20 17:22:26 +0400

    feat: complete migration to Elysia on the server

--- apps/server/src/api/v0/index.ts
+++ apps/server/src/api/v0/index.ts
@@ -1,5 +1,17 @@
 import { Elysia } from "elysia";
 import { auth } from "./auth";
+import { app as routesApiKey } from "./apiKey";
+import { app as routesCustomLesson } from "./customLesson";
 import { app as routesIcs } from "./ics";
+import { app as routesLk } from "./lk";
+import { app as routesNotifications } from "./notifications";
+import { app as routesSchedule } from "./schedule";
 
-export const app = new Elysia({ prefix: "/v0" }).use(auth).use(routesIcs);
+export const app = new Elysia({ prefix: "/v0" })
+  .use(auth)
+  .use(routesIcs)
+  .use(routesSchedule)
+  .use(routesCustomLesson)
+  .use(routesNotifications)
+  .use(routesLk)
+  .use(routesApiKey);
